8c66207c0ebdfa00fdec885563577bdf0fd5dccc
RHrakib1 <rakibhasanmd457@gmail.com>
2024-02-04 13:13:28 +0600

    2nd

---
+++ sort.js
@@ -0,0 +1,9 @@
+// latter onuja e sort hbe string gulo
+// sort 2 poroka
+// ascending ----> soto theke boro onuja e sajabe 
+// descending --->boro theke soto onuja e sajabe
+
+// boro string thakle segula age asbe r soto string gula pore dibe 
+const persons = ['rakib', 'zahid', "Rakib", "Hasan", 'hasib', 'nohib', 'akib', 'bakib', 'sohid'];
+const shortpersone = persons.sort();
+console.log(shortpersone);
